32673e32532516dab76676baac5927677f82f462
Jazzberries <anime.flick.chick@gmail.com>
2022-05-27 16:15:06 +1000

    Minor changes to routes to see if that will help the data wite to the DB

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -9,16 +9,37 @@ router.post('/login', async (req, res) => {
             email: req.body.email,
             password: req.body.password,
         })
+        console.log(userData)
         req.session.save(() => {
             req.session.userId = userData.id;
             req.session.loggedIn = true;
 
-            res.status(200).json({ userData, message: "you are now logged in!" });
+            res.status(200).json({ userData, message: "You are now logged in!" });
         });
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } })
+        console.log(userData);
+
+        if (!userData) {
+            res.status(400).json({ message: "Invalid username or password. Please try again." })
+        }
+        req.session.save(() => {
+            req.session.userId = userData.id;
+            req.session.loggedIn = true;
+
+            res.status(200).json({ userData, message: "You are now logged in!" })
+        })
+
+    } catch (error) {
+        res.status(400).json(error);
+    }
+})
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,5 @@ const homepageRoutes = require('./homepageRoutes')
 router.use('/', homepageRoutes);
 router.use('/api', apiRoutes);
 
+
 module.exports = router;
