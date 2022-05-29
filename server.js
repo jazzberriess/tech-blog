82de879e59b0a6187f9e188131799cec99d6551c
Jazzberries <anime.flick.chick@gmail.com>
2022-05-30 08:12:46 +1000

    Added helper to format dates neatly

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ defaultLayout: 'main' });
-const routes = require('./controllers')
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 //setting up sequelize and sequelize sessions
 const sequelize = require('./config/connection');
@@ -13,6 +13,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 //setting up express
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//set up handlebars with helpers
+const hbs = exphbs.create({ defaultLayout: 'main', helpers });
 
 //session set up
 const sess = {
---
+++ utils/helpers.js
@@ -0,0 +1,7 @@
+module.exports = {
+    format_date: (date) => {
+        const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+        return date.toLocaleDateString('en-GB', dateOptions);
+    }
+};
